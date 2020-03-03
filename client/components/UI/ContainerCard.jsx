de0917acd72c5e626948a1dfebefc46e4ad6b72b
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-02 16:42:54 -0800

    useUser complete

--- client/components/UI/ContainerCard.jsx
+++ client/components/UI/ContainerCard.jsx
@@ -7,10 +7,11 @@ function ContainerCard(props) {
   const bg = props.bg ? props.bg : 'transparent';
 
   return (
-    <Card className="ContainerCard" style={{ height, width, backgroundColor: bg }}>
+    <Card className="ContainerCard d-flex" style={{ height, width, backgroundColor: bg }}>
       {props.header && <Card.Header>{props.header}</Card.Header>}
       {props.body && <Card.Body>{props.body}</Card.Body>}
       {props.footer && <Card.Footer>{props.footer}</Card.Footer>}
+      {props.children}
     </Card>
   );
 }
