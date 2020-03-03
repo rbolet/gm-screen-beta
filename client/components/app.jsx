8d1524cfca64905d302b496712dd365e09a0b3dc
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-03 11:07:41 -0800

    menu component skeleton

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import useWindowDimensions from './logic/useWindowDimensions';
 import Header from './UI/Header';
-import ContainerCard from './UI/ContainerCard';
-import { useUser } from './logic/useConfig';
 
 function App() {
-  const str = `${useUser({ userId: 12 }).user.userId}`;
-  const { height, width, bodyHeight } = useWindowDimensions();
+  const { height, width } = useWindowDimensions();
 
   return (
     <div className="App bg-secondary" style={{ height, width }}>
       <Header/>
-      <div className="app-body container-fluid py-3" style={{ height: bodyHeight, width }}>
-        <ContainerCard percentHeight={100} percentWidth={50} bg="#007bff"
-          body={str}/>
-
-      </div>
 
     </div>
   );
