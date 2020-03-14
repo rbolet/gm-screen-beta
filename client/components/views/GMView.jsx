e1098db52611ba752078ad927ba66039d834da57
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-13 19:50:05 -0700

    removed unnecessary container

--- client/components/views/GMView.jsx
+++ client/components/views/GMView.jsx
@@ -5,11 +5,8 @@ import ContainerCard from '../UI/ContainerCard';
 function GMView(props) {
   return (
     <Body>
-      <ContainerCard percentHeight={100} percentWidth={100} >
-        <ContainerCard className="mr-3" percentHeight={100} percentWidth={66} bg="#343a40"/>
-        <ContainerCard percentHeight={100} percentWidth={32} bg="#343a40"/>
-
-      </ContainerCard>
+      <ContainerCard className="mr-3" percentHeight={100} percentWidth={66} bg="#343a40"/>
+      <ContainerCard percentHeight={100} percentWidth={32} bg="#343a40"/>
     </Body>
   );
 }
