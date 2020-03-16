92dda411eac0de79c3ac3cdfbb3381c68680804e
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-16 16:54:50 -0700

    guest campaign setting automatically

--- client/components/views/Menu.jsx
+++ client/components/views/Menu.jsx
@@ -3,17 +3,18 @@ import Body from '../UI/Body';
 import Login from '../UI/Login';
 import { AppUser } from '../../context/user-context';
 import { Campaign } from '../../context/campaign-context';
-import ChooseCampaign from '../UI/ChooseCampaign';
-
+// import ChooseCampaign from '../UI/ChooseCampaign';
+import { guestCampaign } from '../logic/guestUsers';
 function Menu(props) {
   const { user } = React.useContext(AppUser);
-  const { campaign } = React.useContext(Campaign);
+  const { campaign, updateCampaign } = React.useContext(Campaign);
 
   let CurrentMenu;
   if (!user.userId) {
     CurrentMenu = <Login/>;
   } else if (!campaign.campaignId) {
-    CurrentMenu = <ChooseCampaign/>;
+    // CurrentMenu = <ChooseCampaign/>
+    updateCampaign(guestCampaign);
   }
 
   return (
