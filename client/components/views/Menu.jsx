5e95c14be1408bdd212898723bd77caaff4a2742
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-16 16:34:43 -0700

    added conditional for login

--- client/components/views/Menu.jsx
+++ client/components/views/Menu.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Body from '../UI/Body';
 import Login from '../UI/Login';
+import { AppUser } from '../../context/user-context';
 
 function Menu(props) {
-  const CurrentMenu = <Login/>;
+  const { user } = React.useContext(AppUser);
+  let CurrentMenu;
+  if (!user.userId) {
+    CurrentMenu = <Login/>;
+  }
 
   return (
     <Body>
