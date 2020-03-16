684aea3cf31211dc9b7f078f5ef8f4acf1b0e503
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-16 15:33:33 -0700

    removed header placeholder guest buttons

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,12 @@
 import './Header.css';
 import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
 import { AppUser } from '../context/user-context';
 import { Campaign } from '../context/campaign-context';
 
 function Header(props) {
-  const { user, updateUser } = useContext(AppUser);
-  const { updateCampaign } = useContext(Campaign);
+  const { user } = useContext(AppUser);
+  const { campaign } = useContext(Campaign);
 
   let RoleIcon = null;
   switch (user.userRole) {
@@ -26,6 +25,7 @@ function Header(props) {
       </Navbar.Brand>
       <Navbar.Text className="user-name">{user.userName}</Navbar.Text>
       <Navbar.Text className="role-icon">{RoleIcon}</Navbar.Text>
+      <Navbar.Text className="campaign">{campaign.campaignName}</Navbar.Text>
     </Navbar>
   );
 }
