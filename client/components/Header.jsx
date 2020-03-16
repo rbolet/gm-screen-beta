917341c133d90e0144143f6f4ca8895451bf3cb1
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-16 15:30:55 -0700

    guest info on its own page

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -9,28 +9,6 @@ function Header(props) {
   const { user, updateUser } = useContext(AppUser);
   const { updateCampaign } = useContext(Campaign);
 
-  const guestGMLogin = { userId: 5, userName: 'Guest GM', userRole: 'gm' };
-  const guestPlayerLogin = { userId: 6, userName: 'Guest Player', userRole: 'player' };
-  const guestCampaign = {
-    campaignId: 2,
-    campaignName: 'Guest Campaign',
-    campaignGM: 5,
-    campaignAssets: [
-      {
-        imageId: 4,
-        fileName: '9cf9225a-979f-40cc-a934-e270c1c752a9..jpg',
-        category: 'Environment',
-        alias: 'Village'
-      },
-      {
-        imageId: 5,
-        fileName: '0c00a350-dfbb-4d8f-98af-e0815bbdaefb..png',
-        category: 'Secondary',
-        alias: 'Elf'
-      }
-    ]
-  };
-
   let RoleIcon = null;
   switch (user.userRole) {
     case 'gm':
@@ -48,15 +26,6 @@ function Header(props) {
       </Navbar.Brand>
       <Navbar.Text className="user-name">{user.userName}</Navbar.Text>
       <Navbar.Text className="role-icon">{RoleIcon}</Navbar.Text>
-      <Navbar.Text className="px-2">
-        <Button variant="danger"
-          onClick={() => {
-            updateUser(guestGMLogin); updateCampaign(guestCampaign);
-          }}>Guest GM</Button>
-      </Navbar.Text>
-      <Navbar.Text className="px-2">
-        <Button variant="warning" onClick={() => { updateUser(guestPlayerLogin); }}>Guest Player</Button>
-      </Navbar.Text>
     </Navbar>
   );
 }
---
+++ client/components/logic/guestUsers.js
@@ -0,0 +1,21 @@
+export const guestGMLogin = { userId: 5, userName: 'Guest GM', userRole: 'gm' };
+export const guestPlayerLogin = { userId: 6, userName: 'Guest Player', userRole: 'player' };
+export const guestCampaign = {
+  campaignId: 2,
+  campaignName: 'Guest Campaign',
+  campaignGM: 5,
+  campaignAssets: [
+    {
+      imageId: 4,
+      fileName: '9cf9225a-979f-40cc-a934-e270c1c752a9..jpg',
+      category: 'Environment',
+      alias: 'Village'
+    },
+    {
+      imageId: 5,
+      fileName: '0c00a350-dfbb-4d8f-98af-e0815bbdaefb..png',
+      category: 'Secondary',
+      alias: 'Elf'
+    }
+  ]
+};
