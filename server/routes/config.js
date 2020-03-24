97d4a8c94840c1464023f8e56713a91eb07fcf74
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-23 20:13:40 -0700

    correctly logging lobby user list

--- server/routes/config.js
+++ server/routes/config.js
@@ -3,7 +3,10 @@ const SocketIO = require('./socket-io-server');
 
 router.post('/socket', (req, res) => {
   const user = req.body.user;
-  SocketIO.configSocket(user);
+  const note = SocketIO.configSocket(user) ? 'Connection successfully configured'
+    : 'There was a problem configuring the connection';
+  res.json(['systemNote', note]);
+
 });
 
 module.exports = router;
