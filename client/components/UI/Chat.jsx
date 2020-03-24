2a08904f948eb2951f3f5d2c66f13a41062ab9a5
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-24 13:46:00 -0700

    chat styling started

--- client/components/UI/Chat.jsx
+++ client/components/UI/Chat.jsx
@@ -7,17 +7,22 @@ import Button from 'react-bootstrap/Button';
 export default function Chat(props) {
 
   return (
-    <Accordion>
-      <Card className="chat">
+
+    <Accordion className="chat">
+      <Card className="chat-card bg-dark">
         <Accordion.Collapse eventKey="0">
-          <Card.Body>Chat Window Goes Here</Card.Body>
+          <Card.Body className="p-1">
+            <div className="chat-messages"></div>
+            <div className="chat-user-list"></div>
+          </Card.Body>
         </Accordion.Collapse>
         <Card.Footer className="chat-footer">
-          <Accordion.Toggle as={Button} variant="" eventKey="0">
-            ^
+          <Accordion.Toggle as={Button} variant="secondary" eventKey="0">
+              v
           </Accordion.Toggle>
         </Card.Footer>
       </Card>
     </Accordion>
+
   );
 }
