856016ff96d32d5714c6c9d04a072aecde4ab661
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-27 22:05:41 -0700

    clicking environment image sets environment image, needs styling. Still not interacting with back end.

--- client/components/views/GMView.jsx
+++ client/components/views/GMView.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Body from '@components/UI/Body';
 import ContainerCard from '@components/UI/ContainerCard';
 import ImageGrid from '@components/UI/ImageGrid';
+import { Session } from '@client/context/session-context';
 
-function GMView(props) {
-  const [setSelectedImage] = useState(null);
+export default function GMView(props) {
+  const { session, updateSession } = useContext(Session);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    if (!selectedImage) return;
+    switch (selectedImage.category) {
+      case 'Environment':
+        updateSession({ environmentImageFileName: selectedImage.fileName });
+    }
+  }, [selectedImage]);
+
   return (
     <Body>
-      <ContainerCard percentHeight={100} percentWidth={66} bg="#343a40" shadow={true}/>
+      <ContainerCard percentHeight={100} percentWidth={66} bg="#343a40" shadow={true}>
+        <Environment backgroundImage={session.environmentImageFileName}/>
+      </ContainerCard>
       <ContainerCard percentHeight={100} percentWidth={32} bg="#343a40"shadow={true}>
         <ImageGrid onImageClick={setSelectedImage}/>
       </ContainerCard>
@@ -15,4 +28,8 @@ function GMView(props) {
   );
 }
 
-export default GMView;
+function Environment(props) {
+  return (
+    <div className="environment-image h-100 w-100" style={{ backgroundImage: `url(./images/${props.backgroundImage})`, objectFit: 'contain' }}/>
+  );
+}
