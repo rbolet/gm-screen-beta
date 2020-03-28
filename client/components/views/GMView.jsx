f04533a60ea11ed103f6822a21257cb9740c3329
Rapha Bolet <rapha.bolet@gmail.com>
2020-03-27 21:48:37 -0700

    added selectedImage state to GMView

--- client/components/views/GMView.jsx
+++ client/components/views/GMView.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import Body from '../UI/Body';
-import ContainerCard from '../UI/ContainerCard';
-import ImageGrid from '../UI/ImageGrid';
+import React, { useState } from 'react';
+import Body from '@components/UI/Body';
+import ContainerCard from '@components/UI/ContainerCard';
+import ImageGrid from '@components/UI/ImageGrid';
 
 function GMView(props) {
+  const [setSelectedImage] = useState(null);
   return (
     <Body>
       <ContainerCard percentHeight={100} percentWidth={66} bg="#343a40" shadow={true}/>
       <ContainerCard percentHeight={100} percentWidth={32} bg="#343a40"shadow={true}>
-        <ImageGrid/>
+        <ImageGrid onImageClick={setSelectedImage}/>
       </ContainerCard>
     </Body>
   );
