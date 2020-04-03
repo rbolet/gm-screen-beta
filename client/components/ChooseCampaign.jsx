14afab18fd9cc3d37a2bea8c379d801fad176e4e
Rapha Bolet <rapha.bolet@gmail.com>
2020-04-03 16:51:00 -0700

    styling and adding buttons completed

--- client/components/ChooseCampaign.jsx
+++ client/components/ChooseCampaign.jsx
@@ -24,17 +24,40 @@ export default function ChooseCampaign() {
           onClick={() => { updateCampaign(guestCampaign); }}>Guest Campaign</Button>
       </div>}>
       <div className="campaign-list-container">
-        <Card className="w-100 h-100">
-          <Card.Body className="p-0">
-            <Campaigns/>
+        <Card className="w-100 h-100 bg-dark border-0">
+          <Card.Header>
+            <CampaignListHeader/>
+          </Card.Header>
+          <Card.Body className="p-0 h-100 bg-light mb-2 rounded">
+            <CampaignList/>
           </Card.Body>
+          <Card.Footer className="d-flex justify-content-around p-0">
+            <CampaignListFooter/>
+          </Card.Footer>
         </Card>
       </div>
     </ContainerCard>
   );
 }
 
-function Campaigns() {
+function CampaignListHeader() {
+  const { user } = useContext(AppUser);
+
+  let Icon = <i className="fas fa-dice text-info" />;
+
+  if (user.userRole === 'gm') {
+    Icon = <i className="fas fa-hat-wizard text-danger" />;
+  }
+
+  return (
+    <h5 className="text-light text-center">
+      {`${(user.userRole === 'gm') ? 'Choose' : 'Join'} Campaign`}
+      <span className="mx-2">{Icon}</span>
+    </h5>
+  );
+}
+
+function CampaignList() {
   const { user } = useContext(AppUser);
   const [fetched, setFetched] = useState(false);
   const [CampaignRows, setCampaignRows] = useState([]);
@@ -45,20 +68,22 @@ function Campaigns() {
       setFetched(true);
       setCampaignRows(
         campaigns.map(campaign => {
-          return (<tr
-            key={campaign.campaignId}
-            className={'list-display w-100 border-bottom row-no-gutters'}
-            onClick={() => { }}>
-            <td className="p-2 col">{campaign.campaignName}</td>
-            <td className="d-flex justify-content-end col p-0 m-0">
-              {user.userRole === 'gm' &&
-                <button
-                  className="btn btn-danger"
-                  onClick={() => { }}>
-                  <i className="far fa-trash-alt" />
-                </button>}
-            </td>
-          </tr>);
+          return (
+            <tr
+              key={campaign.campaignId}
+              className='list-display w-100 border-bottom row-no-gutters'
+              onClick={() => { }}>
+              <td className="p-2 col">{campaign.campaignName}</td>
+              <td className="d-flex justify-content-end col p-0 m-0">
+                {user.userRole === 'gm' &&
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => { }}>
+                    <i className="far fa-trash-alt" />
+                  </button>}
+              </td>
+            </tr>
+          );
         })
       );
     }
@@ -68,15 +93,40 @@ function Campaigns() {
     return <Loading/>;
   } else if (CampaignRows.length) {
     return (
-      <table className="m-0 w-100" id="campaign-list">
-        <tbody id="campaign-table-body">
-          { CampaignRows }
+      <table className="m-0 w-100 rounded">
+        <tbody>
+          {CampaignRows}
         </tbody>
       </table>
     );
   }
 
   return <EmptySet/>;
+}
+
+function CampaignListFooter() {
+  const { user } = useContext(AppUser);
+
+  return (
+    <>
+      <Button variant="outline-success" className="footer-button"
+        onClick={() => {}}>
+        <i className={`fas ${user.userRole === 'gm' ? 'fa-plus-circle' : 'fa-redo-alt'}`} />
+        <p className="button-text m-0">{user.userRole === 'gm' ? 'New' : 'Refresh'}</p>
+      </Button>
+      {user.userRole === 'gm' &&
+        <Button variant="secondary" className="footer-button"
+          onClick={() => {}}>
+          <i className="fas fa-file-upload" />
+          <p className="button-text m-0">Configure</p>
+        </Button>}
+      <Button variant="success" className="footer-button"
+        onClick={() => {}}>
+        <i className="fas fa-play" />
+        <p className="button-text m-0">{user.userRole === 'gm' ? 'Launch' : 'Join'}</p>
+      </Button>
+    </>
+  );
 }
 
 function useCampaigns(fetched) {
