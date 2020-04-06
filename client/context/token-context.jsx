691b80f4b32588ea3107401e538cb078c6f21fa6
Rapha Bolet <rapha.bolet@gmail.com>
2020-04-06 16:07:45 -0700

    moved token state to context, added hidden

--- client/context/token-context.jsx
+++ client/context/token-context.jsx
@@ -3,21 +3,34 @@ import React, { useState } from 'react';
 export const Token = React.createContext(null);
 
 export function TokenContext(props) {
-  const [token, setToken] = useState({});
+  const [tokenId, setTokenId] = useState(null);
+  const [imageFileName, setImageFileName] = useState(null);
+  const [tokenName, setTokenName] = useState('');
+  const [tokenDetails, setTokenDetails] = useState('');
+  const [hidden, setHidden] = useState(0);
   const [loading, isLoading] = useState(false);
 
   const newToken = async image => {
-    setToken({
-      tokenId: 'new',
-      imageFileName: image.fileName,
-      tokenName: image.alias,
-      tokenDetails: '',
-      hidden: 0
-    });
+    setTokenId('new');
+    setImageFileName(image.fileName);
+    setTokenName(image.alias);
   };
 
-  const modifyToken = async newTokenState => {
-    setToken(Object.assign(token, newTokenState));
+  const modifyToken = async newState => {
+    Object.keys(newState).forEach(key => {
+      switch (key) {
+        case 'tokenId':
+          setTokenId(newState.tokenId); break;
+        case 'tokenName':
+          setTokenName(newState.tokenName); break;
+        case 'imageFileName':
+          setImageFileName(newState.imageFileName); break;
+        case 'tokenDetails':
+          setTokenDetails(newState.tokenDetails); break;
+        case 'hidden':
+          setHidden(newState.hidden); break;
+      }
+    });
   };
 
   const updateToken = async (newState, isNew) => {
@@ -25,12 +38,17 @@ export function TokenContext(props) {
     if (isNew) {
       newToken(newState).then(p => isLoading(false));
     } else {
-      if (newState === 'clear') setToken({});
-      modifyToken(newState)
-        .then(p => isLoading(false));
+      if (newState === 'clear') {
+        setTokenId(null); setImageFileName(null); setTokenName('');
+        setTokenDetails(''); setHidden(0);
+      } else {
+        modifyToken(newState)
+          .then(p => isLoading(false));
+      }
     }
   };
 
+  const token = { tokenId, imageFileName, tokenName, tokenDetails, hidden };
   return (
     <Token.Provider value={{ loading, token, updateToken } }>{props.children}</Token.Provider>
   );
