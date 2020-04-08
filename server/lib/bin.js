c9a77e0b41b19e6090dd065e11e5209564de566e
Rapha Bolet <rapha.bolet@gmail.com>
2020-04-08 12:38:10 -0700

    adding hidden tokens to seperate array in backend

--- server/lib/bin.js
+++ server/lib/bin.js
@@ -4,21 +4,26 @@ exports.activeGameSessions = [];
 exports.userSocketList = {};
 
 exports.buildSession = function (sessionId) {
-  let tokens = [];
+  const tokens = []; const hiddenTokens = [];
   return new Promise(resolve => {
     db.query(`SELECT * FROM tokens WHERE sessionId = ${sessionId}`)
       .then(([rows]) => {
         for (const token of rows) {
           token.hidden = Boolean(token.hidden);
+          if (token.hidden) {
+            hiddenTokens.push(token);
+          } else {
+            tokens.push(token);
+          }
         }
-        tokens = rows;
         return db.query(`SELECT * FROM sessions WHERE sessionId = ${sessionId}`);
       })
       .then(([result]) => {
         return {
           sessionId: result[0].sessionId,
           environmentImageFileName: result[0].environmentImageFileName,
-          tokens
+          tokens,
+          hiddenTokens
         };
       })
       .then(done => resolve(done));
