be1b498a93624ad787af904a33ab04d5f441540d
Rapha Bolet <rapha.bolet@gmail.com>
2020-04-08 16:57:53 -0700

    temporarily auto-assigning guest gm and to guest player 1

--- server/routes/config.js
+++ server/routes/config.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 const SocketIO = require('./socket-io-server');
 
 router.post('/socket', (req, res) => {
-  const user = req.body.user;
+  const user = req.body.user.userId.length > 3 ? assignId(req.body.user) : req.body.user;
   const note = SocketIO.configSocket(user) ? 'Connection successfully configured'
     : 'There was a problem configuring the connection';
   res.json(['systemNote', note]);
 
 });
+
+function assignId(guestUser) {
+  guestUser.userId = guestUser.userRole === 'gm' ? 5 : 6;
+  return guestUser;
+}
 
 module.exports = router;
