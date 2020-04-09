dd7fb998e7d13a741c01f633051612548a80342d
Rapha Bolet <rapha.bolet@gmail.com>
2020-04-08 20:26:09 -0700

    visibleto component toggling properly based on front end data

--- client/components/UI/SelectVisibleTo.jsx
+++ client/components/UI/SelectVisibleTo.jsx
@@ -11,25 +11,32 @@ import { Token } from '@client/context/token-context';
 export default function SelectVisibleTo() {
   const { campaign } = useContext(Campaign);
   const { token, updateToken } = useContext(Token);
+  const [toggled, setToggled] = useState(token.visibleTo.map(user => user.userId) || []);
   const [ToggleButtons, setToggleButtons] = useState(null);
 
   useEffect(() => {
     const players = campaign.roomUserList.filter(user => user.userRole === 'player');
 
+    if (token.visibleTo) setToggled(token.visibleTo.map(user => user.userId));
+
     if (players.length) {
       setToggleButtons(players.map(user => {
         return (
           <ToggleButton size="sm" key={user.userId} value={user.userId} className="text-dark"
-            variant="outline-info" disabled={!token.hidden}>{user.userName}</ToggleButton>
+            variant="outline-info" disabled={!token.hidden}
+          >{user.userName}</ToggleButton>
         );
       }));
     }
-  }, [campaign.roomUserList]);
+  }, [campaign.roomUserList, token.visibleTo]);
 
   return (
     <ButtonToolbar className="visible-toolbar">
       <ButtonGroup>
-        <ToggleButtonGroup type="checkbox" onChange={() => { }}>
+        <ToggleButtonGroup type="checkbox" defaultValue={toggled}
+          onChange={() => {
+            setToggled([event.target].flat());
+          }}>
           {ToggleButtons}
         </ToggleButtonGroup>
         <Button variant="outline-info" active={!token.hidden} size="sm"
