f8442f6be780c6cabc27f291c8e07bf05fa3b477
Rapha Bolet <rapha.bolet@gmail.com>
2020-04-09 18:06:55 -0700

    new backend approach (both hidden and non-hidden tokens come together with session updates

--- server/lib/bin.js
+++ server/lib/bin.js
@@ -1,23 +1,38 @@
 const db = require('../_config');
-const SocketIO = require('../routes/socket-io-server');
 
 exports.activeGameSessions = [];
 exports.userSocketList = {};
 
 exports.buildSession = function (sessionId) {
-  const tokens = []; const hiddenTokens = [];
+  const joinedQuery = `SELECT tokenId,
+                              sessionId,
+                              imageFileName,
+                              tokenName,
+                              tokenDetails,
+                              hidden,
+                              JSON_ARRAYAGG(userId) AS visibleTo
+                      FROM (SELECT tokens.tokenId AS tokenId,
+                              sessionId,
+                              imageFileName,
+                              tokenName,
+                              tokenDetails,
+                              hidden,
+                              userId FROM tokens
+                            JOIN tokenVisibleTo ON tokens.tokenId = tokenVisibleTo.tokenId) AS combined
+                      WHERE combined.sessionId = ${sessionId}
+                      GROUP BY tokenId;`;
+  // db.query(joinedQuery).then(([rows]) => rows).catch(err => console.error(err));
+  let tokens = [];
   return new Promise(resolve => {
-    db.query(`SELECT * FROM tokens WHERE sessionId = ${sessionId}`)
-      .then(([rows]) => {
-        for (const token of rows) {
-          token.hidden = Boolean(token.hidden);
-          if (token.hidden) {
-            hiddenTokens.push(token);
-          } else {
-            tokens.push(token);
-          }
-        }
-        buildVisibleToList(hiddenTokens);
+    db.query(`SELECT * FROM tokens WHERE sessionId = ${sessionId} AND hidden = 0;`)
+      .then(([nonHiddenTokens]) => {
+        db.query(joinedQuery)
+          .then(([hiddenTokens]) => {
+            tokens = [...hiddenTokens, ...nonHiddenTokens];
+            for (const token of tokens) {
+              token.hidden = Boolean(token.hidden);
+            }
+          });
         return db.query(`SELECT * FROM sessions WHERE sessionId = ${sessionId}`);
       })
       .then(([result]) => {
@@ -30,11 +45,3 @@ exports.buildSession = function (sessionId) {
       .then(done => resolve(done));
   });
 };
-
-function buildVisibleToList(hiddenTokens) {
-  hiddenTokens.forEach(token => {
-    const groupByQuery = `SELECT userId FROM tokenVisibleTo WHERE tokenId = ${token.tokenId};`;
-    db.query(groupByQuery)
-      .then(([rows]) => { SocketIO.updateTokenVisibility(token, rows); });
-  });
-}
