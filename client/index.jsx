b259a748c8a6338a10c8ec11de2c818d133b71af
Ubuntu <ubuntu@ip-172-31-2-253.us-west-1.compute.internal>
2020-04-17 21:40:45 +0000

    fixed casing problem with import

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '@components/App';
+import App from '@components/app';
 import ConfigContext from '@client/context/config-context';
 
 ReactDOM.render(
