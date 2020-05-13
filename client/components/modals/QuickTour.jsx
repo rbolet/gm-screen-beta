bf2e742c7264b3534f8232d8368f51d3230556bc
Rapha Bolet <rapha.bolet@gmail.com>
2020-05-13 11:45:19 -0700

    moved role view content into functions

--- client/components/modals/QuickTour.jsx
+++ client/components/modals/QuickTour.jsx
@@ -9,15 +9,14 @@ import CloseButton from '@components/UI/CloseButton';
 import ContainerCard from '@components/UI/ContainerCard';
 
 export default function QuickTour(props) {
-  // const [page, setPage] = useState(1);
-  // const lastPage = 6;
+
   const headerText = 'Welcome to GM Screen';
 
   return (
     <Portal>
       <ModalBackground>
         <ContainerCard
-          percentHeight={100}
+          percentHeight={98}
           percentWidth={66}
           bg="#6c757d" shadow
           header={
@@ -29,7 +28,6 @@ export default function QuickTour(props) {
         >
           <Tab.Container id="roles" defaultActiveKey="overview">
             <Row className="no-gutters">
-              {/* <Col sm={3}> */}
               <Nav variant="pills">
                 <Nav.Item>
                   <Nav.Link className="text-light" eventKey="overview">Overview</Nav.Link>
@@ -41,30 +39,45 @@ export default function QuickTour(props) {
                   <Nav.Link className="text-light" eventKey="player">Player</Nav.Link>
                 </Nav.Item>
               </Nav>
-              {/* </Col> */}
             </Row>
             <Row className="no-gutters">
-              {/* <Col sm={9}> */}
               <Tab.Content>
                 <Tab.Pane eventKey="gm">
-                  <p>The GM content goes here</p>
+                  <GM/>
                 </Tab.Pane>
                 <Tab.Pane eventKey="player">
-                  <p>The Player content goes here</p>
+                  <Player/>
                 </Tab.Pane>
                 <Tab.Pane eventKey="overview">
-                  <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GM-Screen is not a game. It is an application to supplement the storytelling and enhance
-                  the immersion of traditional paper-and-dice role playing games,
-                  allowing Game Masters to share images in real time with the rest of the player group.
-                  Since GM-Screen does not use any game mechanics, it is useable with any system, game,
-                    or any situation imaginable where visual aids can add to a group&apos;s shared experience.</p>
+                  <Overview/>
                 </Tab.Pane>
               </Tab.Content>
-              {/* </Col> */}
             </Row>
           </Tab.Container>
         </ContainerCard>
       </ModalBackground>
     </Portal>
+  );
+}
+
+function Overview() {
+  return (
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GM-Screen is not a game. It is an application to supplement the storytelling and enhance
+    the immersion of traditional paper-and-dice role playing games,
+    allowing Game Masters to share images in real time with the rest of the player group.
+    Since GM-Screen does not use any game mechanics, it is useable with any system, game,
+                    or any situation imaginable where visual aids can add to a group&apos;s shared experience.</p>
+  );
+}
+
+function GM() {
+  return (
+    <p>GM Content</p>
+  );
+}
+
+function Player() {
+  return (
+    <p>Player Content</p>
   );
 }
